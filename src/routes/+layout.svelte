5b3f2447c9565dc67c3ed661bc4305bbdee45156
Arseniy <arsenijbojko35@gmail.com>
2023-06-06 17:19:38 +0500

    add blog

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,26 +3,27 @@
         {
             title: "Обо мне",
             href: "/about",
+            isShow: true,
         },
-
         {
             title: "Блог",
             href: "/blog",
+            isShow: true,
         },
-
         {
             title: "Портфолио",
             href: "/project",
+            isShow: true,
         },
-
         {
             title: "Контакты",
             href: "/contact",
+            isShow: true,
         },
-
         {
             title: "temp",
             href: "/temp",
+            isShow: false,
         },
     ];
 </script>
@@ -32,7 +33,9 @@
         <a href="/" class="nav-logo" title="Back to Homepage">Арсений Бойко</a>
         <div class="nav-links">
             {#each nav as link}
-                <a href={link.href} class="link">{link.title}</a>
+                {#if link.isShow}
+                    <a href={link.href} class="link">{link.title}</a>
+                {/if}
             {/each}
         </div>
     </div>
